d4c1ca28023116ba6238ce10ae06dc4b774bd007
Clement <clement.allirot@external.totalenergies.com>
2024-06-12 11:12:18 +0200

    Etape 8 : Le panier

---
+++ src/Classe/Cart.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace App\Classe;
+
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class Cart
+{
+
+    public function __construct(
+        private RequestStack $requestStack,
+    ) {
+        // Accessing the session in the constructor is *NOT* recommended, since
+        // it might not be accessible yet or lead to unwanted side-effects
+        // $this->session = $requestStack->getSession();
+    }
+
+    /*
+        Fonction permettant l'ajout de produit au panier
+    */
+    public function add($product)
+    {
+        // obtenir la session en cours
+        $cart = $this->getCart();
+
+        // obtenir la quantité actuelle et y ajouter + 1
+        $qty = 1;
+        if (isset($cart[$product->getId()]['qty'])) {
+            $qty = $cart[$product->getId()]['qty'] + 1;
+        }
+
+        // Mettre à jour le produit et ses infos dans la variable $cart
+        $cart[$product->getId()] = [
+            'object' => $product,
+            'qty' => $qty
+        ];
+
+        // Enregistrer la $cart dans session
+        $this->requestStack->getSession()->set('cart', $cart);
+    }
+
+
+    /*
+        Fonction permettant la suppression d'un produit au panier
+    */
+    public function decrease($idProduct)
+    {
+        // obtenir la session en cours
+        $cart = $this->getCart();
+
+        // erreur si cart n'existe pas en session
+        if (!isset($cart[$idProduct]['qty'])) {
+            return "error";
+        }
+
+        // obtenir la quantité actuelle
+        $qty = $cart[$idProduct]['qty'];
+
+        // si qty == 1 on supprime le produit
+        if($qty == 1){
+            unset($cart[$idProduct]);
+            // Enregistrer la $cart dans session
+            $this->requestStack->getSession()->set('cart', $cart);
+            return;
+        }
+
+        // obtenir la quantité actuelle et y enlever - 1
+        $qty = $cart[$idProduct]['qty'] - 1;
+
+        // Mettre à jour le produit et ses infos dans la variable $cart
+        $cart[$idProduct]['qty'] = $qty;
+
+        // Enregistrer la $cart dans session
+        $this->requestStack->getSession()->set('cart', $cart);
+    }
+
+    /*
+        Fonction retournant le nombre total de produit au panier
+    */
+    public function fullQuantity(){
+
+        // obtenir la session en cours
+        $cart = $this->getCart();
+
+        // erreur si cart n'existe pas en session
+        if (!isset($cart)) {
+            return "error";
+        }
+
+        //init la qty à 0;
+        $qty = 0;
+
+        // foreach les cart pour ajouter toutes les qty à $qty
+        foreach ($cart as $idProduct => $value) {
+            $qty += $value['qty'];
+        }
+
+        return $qty;
+    }
+
+    /*
+        Fonction retournant le prix TTC total des produits au panier
+    */
+    public function getTotalWt(){
+
+        // obtenir la session en cours
+        $cart = $this->getCart();
+
+        // erreur si cart n'existe pas en session
+        if (!isset($cart)) {
+            return "error";
+        }
+
+        //init le prix with tax à 0;
+        $totalWt = 0;
+
+        // foreach les cart pour ajouter toutes les qty à $qty
+        foreach ($cart as $value) {
+            $totalWt += ($value['object']->getPriceWt() * $value['qty']);
+        }
+
+        return $totalWt;
+    }
+
+    /*
+        Fonction permettant la suppression de tous les produits du panier
+    */
+    public function remove(){
+        // Supprimer l'object cart dans la session
+        $this->requestStack->getSession()->remove('cart');
+    }
+
+    /*
+        Fonction permettant d'obtenir l'objet cart en session
+    */
+    public function getCart()
+    {
+        return $this->requestStack->getSession()->get('cart');
+    }
+}
---
+++ src/Controller/CartController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Controller;
+
+use App\Classe\Cart as Cart;
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class CartController extends AbstractController
+{
+    #[Route('/mon-panier', name: 'app_cart')]
+    public function index(Cart $cart): Response
+    {
+        return $this->render('cart/index.html.twig', [
+            'cart' => $cart->getCart(),
+            'totalWt' => $cart->getTotalWt()
+        ]);
+    }
+
+    #[Route('/cart/add/{id}', name: 'app_cart_add')]
+    public function add(
+        $id,
+        Cart $cart,
+        ProductRepository $productRepository,
+        Request $request
+    ) {
+        // derniere url visitée
+        $referer = $request->headers->get("referer");
+
+        $product = $productRepository->findOneById($id);
+
+        $cart->add($product);
+
+        $this->addFlash(
+            'success',
+            'Produit correctement ajouté à votre panier'
+        );
+
+        return $this->redirect($referer);
+    }
+
+    #[Route('/cart/decrease/{id}', name: 'app_cart_decrease')]
+    public function decrease(
+        $id,
+        Cart $cart,
+    ) {
+        $cart->decrease($id);
+
+        $this->addFlash(
+            'success',
+            'Produit correctement supprimé à votre panier'
+        );
+
+        return $this->redirectToRoute("app_cart");
+    }
+
+    #[Route('/cart/remove', name: 'app_cart_remove')]
+    public function remove(Cart $cart)
+    {
+        $cart->remove();
+
+        $this->addFlash(
+            'success',
+            'Votre panier a bien été vidé'
+        );
+
+        return $this->redirectToRoute('app_home');
+    }
+}
